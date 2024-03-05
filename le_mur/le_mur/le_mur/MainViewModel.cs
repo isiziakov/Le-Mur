ab20fbd2b1f088ef3d6b2018a22e7910b085e008
ermolenkooo <dashalisa2001@mail.ru>
2024-03-05 21:53:19 +0300

    попытка нарисовать главное окно номер один

--- le_mur/le_mur/le_mur/MainViewModel.cs
+++ le_mur/le_mur/le_mur/MainViewModel.cs
@@ -4,65 +4,54 @@ using System.Collections.ObjectModel;
 using System.Text;
 using Xamarin.Forms;
 using le_mur.TelegramApi;
+using le_mur.Models;
 
 namespace le_mur
 {
     public class MainViewModel : BaseViewModel
     {
-        public Command DownloadCommand { get; }
+        public Command LikeCommand { get; }
+        public Command CommentCommand { get; }
+        public Command FavouritesCommand { get; }
 
-        private string code;
-        public string Code
+        private ObservableCollection<Post> posts;
+        public ObservableCollection<Post> Posts
         {
-            get { return code; }
+            get { return posts; }
             set
             {
-                if (code != value)
+                if (posts != value)
                 {
-                    code = value;
-                    OnPropertyChanged("Code");
-                }
-            }
-        }
-
-        private ObservableCollection<string> groups;
-        public ObservableCollection<string> Groups
-        {
-            get { return groups; }
-            set
-            {
-                if (groups != value)
-                {
-                    groups = value;
-                    OnPropertyChanged("Groups");
+                    posts = value;
+                    OnPropertyChanged("Posts");
                 }
             }
         }
 
         public MainViewModel()
         {
-            DownloadCommand = new Command(onDownloadCommand);
+            LikeCommand = new Command(OnLikeCommand);
+            CommentCommand = new Command(OnCommentCommand);
+            FavouritesCommand = new Command(OnFavouritesCommand);
 
-            Groups = new ObservableCollection<string>();
+            Posts = new ObservableCollection<Post>();
+            Posts.Add(new Post { Id = 1, Image = "post1.png", ChanelImage = "chanel.gif", ChanelName = "ИГЭУ имени В.И. Ленина", ChanelTheme = "Университет" });
+            Posts.Add(new Post { Id = 2, Image = "post2.png", ChanelImage = "chanel.gif", ChanelName = "ИГЭУ имени В.И. Ленина", ChanelTheme = "Университет" });
         }
 
-        private async void onDownloadCommand()
+        public void OnLikeCommand(object obj)
         {
-            await TelegramApi.ApiOperations.MakeClient();
-            var res = await TelegramApi.ApiOperations.Auth();
-            if (res == "")
-            {
-                foreach (string d in await ApiOperations.GetChannels())
-                    Groups.Add(d);
-            }
-            else
-            {
-                if (await ApiOperations.MakeAuth(res, Code))
-                {
-                    foreach (string d in await ApiOperations.GetChannels())
-                        Groups.Add(d);
-                }
-            }
+            
+        }
+
+        public void OnCommentCommand(object obj)
+        {
+            
+        }
+
+        public void OnFavouritesCommand(object obj)
+        {
+            
         }
     }
 }
--- le_mur/le_mur/le_mur/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ le_mur/le_mur/le_mur/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace le_mur {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ListView postsList;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            postsList = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "postsList");
         }
     }
 }
