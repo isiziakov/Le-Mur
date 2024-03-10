add1ec01f1aa01915cb47c2a9ac7e85077b67574
isiziakov <ivan.siziakov@mail.ru>
2024-03-10 15:01:56 +0300

    Добавлено получение фото канала и текста сообщений

---
+++ le_mur/le_mur/le_mur/Model/ChatInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace le_mur.Model
+{
+    internal class ChatInfo
+    {
+    }
+}
--- le_mur/le_mur/le_mur/NetworkCalling/TelegramApi.cs
+++ le_mur/le_mur/le_mur/NetworkCalling/TelegramApi.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using le_mur.Consts;
+using le_mur.Model;
+using TL;
 
 namespace le_mur.NetworkCalling
 {
@@ -38,10 +41,43 @@ namespace le_mur.NetworkCalling
             }
         }
 
-        static async public Task<List<string>> GetChatsTitles()
+        static async public Task<List<string>> GetChatsTitles() // Depricated
         {
             var res = await client.Messages_GetAllChats();
             return res.chats.Select(i => i.Value.Title).ToList();
+        }
+
+        static async public Task<List<ChatInfo>> GetChatsInfo()
+        {
+            var chats = new List<ChatInfo>();
+            var res = await client.Messages_GetAllChats();
+            foreach (var chat in res.chats)
+            {
+                chats.Add(new ChatInfo(chat.Value, chat.Value.Title, await GetChatPhoto(chat.Value)));
+            }
+            return chats;
+        }
+
+        static async public Task<byte[]> GetChatPhoto(IPeerInfo peer)
+        {
+            MemoryStream ms = new MemoryStream();
+            await client.DownloadProfilePhotoAsync(peer, ms);
+            return ms.ToArray();
+        }
+
+        static async public Task<List<MessageInfo>> GetMessages(InputPeer peer, int offsetId = 0)
+        {
+            var res = new List<MessageInfo>();
+            var msgs = await client.Messages_GetHistory(peer, offsetId);
+            foreach (var msg in msgs.Messages)
+            {
+                if (msg is Message)
+                {
+                    var message = (msg as Message);
+                    res.Add(new MessageInfo(message.ID, message.message));
+                }
+            }
+            return res;
         }
     }
 }
